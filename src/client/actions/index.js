083987a38f5b49b45c9381d4e79177d730f54f29
sadjad <sghgh1996@yahoo.com>
2018-07-11 00:28:40 +0430

    made an instance of axios for client side

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
 export const FETCH_USERS = 'fetch_users';
-export const fetchUsers = () => async (dispatch) => {
-    const res = await axios.get('http://localhost:8000/ssrapi/users');
-    dispatch({
-        type: FETCH_USERS,
-        payload: res.data.payload
-    });
+export const fetchUsers = () => async (dispatch, getState, api) => {
+  const res = await api.get('/users');
+  dispatch({
+    type: FETCH_USERS,
+    payload: res
+  });
 };
--- src/client/client.js
+++ src/client/client.js
@@ -9,12 +9,19 @@ import {Provider} from 'react-redux';
 import {renderRoutes} from 'react-router-config';
 import Routes from './Routes';
 import reducers from './reducers/index';
+import axios from 'axios';
+
+// This is an instance for client side. Every request from browser will meet this
+// version of axios.
+const axiosInstance = axios.create({
+  baseURL: '/api'
+});
 
 // Store for client side
 const store = createStore(
     reducers,
     window.INITIAL_STATE,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk.withExtraArgument(axiosInstance)
 );
 // Just in browser
 ReactDOM.hydrate(
